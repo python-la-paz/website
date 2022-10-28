dbf64f95b04ab2e13dddd8264aa472cf1fab99e6
Jonathan Villanueva <me@jevillanueva.dev>
2022-10-28 13:34:32 -0400

    to prod sample

--- website/website/settings/base.py
+++ website/website/settings/base.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "website.urls"
--- website/website/settings/production.py
+++ website/website/settings/production.py
@@ -1,7 +1,19 @@
 from .base import *
 
+print ("PROD")
 DEBUG = False
-
+SECRET_KEY = os.getenv("APP_SECRET_KEY", 'djang-inse-cure-*sl%whcx#i*k+gqy+(^2c(b57v22##+ks4*$zyilfxpz^gnny(')
+print (SECRET_KEY)
+CSRF_TRUSTED_ORIGINS = os.getenv("APP_CSRF_TRUSTED_ORIGINS", 'http://localhost,https://localhost').split(',')
+ALLOWED_HOSTS = ['*'] 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+STATICFILES_STORAGE= 'django.contrib.staticfiles.storage.StaticFilesStorage'
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_CSS_HASHING_METHOD = 'content'
 try:
     from .local import *
 except ImportError:
